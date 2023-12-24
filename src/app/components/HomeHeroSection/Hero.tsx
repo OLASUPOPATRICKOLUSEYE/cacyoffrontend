d989bd5f47ffa5e974de3d2d3e4307dd3c6d7057
OLASUPOPATRICK <olasupoolayemi415@gmail.com>
2023-12-24 19:27:49 +0100

    hero fix

--- src/app/components/HomeHeroSection/Hero.tsx
+++ src/app/components/HomeHeroSection/Hero.tsx
@@ -40,7 +40,7 @@ const HeroSection: React.FC = () => {
   const { backgroundColors, welcomeMessage, ctaButton } = data;
 
   return (
-    <div className="hero-section relative h-screen py-32">
+    <div className="hero-section relative min-h-screen py-32">
       {backgroundColors.map((color: string, index: number) => (
         <div
           key={index}
@@ -49,14 +49,20 @@ const HeroSection: React.FC = () => {
           <div className="absolute inset-0 bg-gray-900 opacity-50"></div>
 
           <div className="text-center relative z-10 pt-20">
-            <h1 className="text-4xl md:text-6xl font-bold mb-4">{welcomeMessage.title}</h1>
-            <p className="text-lg md:text-xl mb-6">{welcomeMessage.subtitle}</p>
-            <p className="text-lg md:text-xl mb-6 px-2">{welcomeMessage.description}</p>
+            <h1 className="text-2xl md:text-4xl lg:text-5xl font-bold mb-4">
+              {welcomeMessage.title}
+            </h1>
+            <p className="text-base md:text-lg lg:text-xl mb-4">
+              {welcomeMessage.subtitle}
+            </p>
+            <p className="text-base md:text-lg lg:text-xl mb-4 px-2">
+              {welcomeMessage.description}
+            </p>
           </div>
 
           <a
             href={ctaButton.link}
-            className="mt-4 px-6 py-3 bg-gray-900 text-white font-bold rounded transition duration-300 hover:bg-gray-300 hover:text-black relative z-10"
+            className="mt-4 px-4 md:px-6 py-2 md:py-3 bg-gray-900 text-white font-bold rounded transition duration-300 hover:bg-gray-300 hover:text-black relative z-10"
           >
             {ctaButton.text}
           </a>
