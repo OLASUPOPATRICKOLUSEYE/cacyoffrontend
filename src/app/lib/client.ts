1068667557a6bf9f215c37b0352286c6cf2e07e1
OLASUPOPATRICK <olasupoolayemi415@gmail.com>
2023-12-24 15:58:38 +0100

    corrected

--- src/app/lib/client.ts
+++ src/app/lib/client.ts
@@ -2,13 +2,15 @@
 import { createClient } from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
-export const client = createClient({
+const client = createClient({
   projectId: process.env.NEXT_JS_APP_SANITY_PROJECT_ID,
   dataset: 'production',
   apiVersion: '2022-03-07',
   useCdn: true,
   token: process.env.NEXT_JS_APP_SANITY_TOKEN,
 });
+
+export default client;
 
 const builder = imageUrlBuilder(client);
 
