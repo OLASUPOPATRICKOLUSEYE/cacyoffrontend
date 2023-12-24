5477dcbd0d1099cb7c4e2f14122e9265b4eb04a0
OLASUPOPATRICK <olasupoolayemi415@gmail.com>
2023-12-24 15:42:56 +0100

    client adjust

--- src/app/lib/client.ts
+++ src/app/lib/client.ts
@@ -1,7 +1,8 @@
 // frontend/client.js
 import { createClient } from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
 
-const client = createClient({
+export const client = createClient({
   projectId: process.env.NEXT_JS_APP_SANITY_PROJECT_ID,
   dataset: 'production',
   apiVersion: '2022-03-07',
@@ -9,6 +10,6 @@ const client = createClient({
   token: process.env.NEXT_JS_APP_SANITY_TOKEN,
 });
 
-export default client;
+const builder = imageUrlBuilder(client);
 
-
+export const urlFor = (source:any) => builder.image(source);
