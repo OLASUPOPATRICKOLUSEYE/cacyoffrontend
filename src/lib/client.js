2b04b5b0486e3651a95d7daef7c8abff5367a88c
OLASUPOPATRICK <olasupoolayemi415@gmail.com>
2023-12-24 14:09:32 +0100

    changes version

--- src/lib/client.js
+++ src/lib/client.js
@@ -4,7 +4,7 @@ import {createClient} from '@sanity/client';
 const client = createClient({
   projectId: 'inrjwceq',
   dataset: 'production',
-  apiVersion: '2023-03-04',
+  apiVersion: '2022-03-07',
   useCdn: true,
   token: 'skOTqBGSpzfMfvzGJzlBoYfPbZp61FOHn2EYkWY7sCQOUG5MBunDP3dSTwBpVz6H8JbPAcqnBdiNpkCmkXIwX5f5L8voQRtbiZRjO2TR5NJGob579Q2JZBv4KwWy9dbRQa39RZ98dMTZ4dSgRHWVLlJZjwAVommJ1WZuBRaxjm8Ya245eGXr',
 });
